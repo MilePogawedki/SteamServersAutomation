68c7eed497ef1fd60e62b56a9c81eb248ab09c97
Wojtini <whitem200@gmail.com>
2023-05-13 16:55:47 +0200

    d

--- steam_server_manager/game_servers/arma_3_server/arma_3_server.py
+++ steam_server_manager/game_servers/arma_3_server/arma_3_server.py
@@ -27,7 +27,7 @@ class Arma3Server(SteamGameServer):
     def prepare_bash_file(mods: Dict[int, WorkshopMod]) -> None:
         file: str = "/home/steam/run_arma3_server.sh"
         mod_string = (f"""mods/{mod.simplified_mod_name};""" for mod in mods.values())
-        with open(file) as run_file:
+        with open(file, "w") as run_file:
             run_file.write(
                 "/home/steam/steamapps/arma_3_server/arma3server"
                 " -name=server -config=server.cfg"
