76c16d208f1560705aaf71fd4f3bded73113b6ee
Wojtini <whitem200@gmail.com>
2023-05-13 16:54:00 +0200

    anon

--- steam_server_manager/common/steam_credentials.py
+++ steam_server_manager/common/steam_credentials.py
@@ -3,11 +3,12 @@ import logging
 
 from steam_totp import generate_twofactor_code_for_time
 
+STEAM_CREDENTIALS = ("anonymous", "")
 if "STEAM_USERPASS" not in os.environ.keys():
     logging.getLogger("steam_credentials").warning("No credentials in env variables STEAM_USERPASS (ex. 'user,pass')")
-
-STEAM_CREDENTIALS = tuple(os.environ["STEAM_USERPASS"].split(":"))
-SECRET = os.environ["FA_SECRET"]
+else:
+    STEAM_CREDENTIALS = tuple(os.environ["STEAM_USERPASS"].split(":"))
+    SECRET = os.environ["FA_SECRET"]
 
 
 def get_token():
